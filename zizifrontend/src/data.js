d13c6ac03fcde6d1d9d69bcfd5674f66fab43d86
Chibueze Kingsley Casper <84658672+CasperKingsley@users.noreply.github.com>
2021-05-31 12:11:24 -0700

    new changes

---
+++ zizifrontend/src/data.js
@@ -0,0 +1,88 @@
+// const data = {
+//     products: [
+//         {
+//             _id: '1',
+//             name: 'Nike Slim shirts',
+//             category: 'Shirts',
+//             Image: '/images/product1.jpg',
+//             price: 120,
+//             brand: 'Nike',
+//             rating: 4.5,
+//             numReviews: 10,
+//             description: 'high quality product',
+
+//         },
+//         {
+//             _id: '1',
+//             name: 'Nike Slim shirts',
+//             category: 'Shirts',
+//             Image: '/images/product1.jpg',
+//             price: 120,
+//             brand: 'Nike',
+//             rating: 4.5,
+//             numReviews: 10,
+//             description: 'high quality product',
+
+//         },
+//         {
+//             _id: '1',
+//             name: 'Nike Slim shirts',
+//             category: 'Shirts',
+//             Image: '/images/product1.jpg',
+//             price: 120,
+//             brand: 'Nike',
+//             rating: 4.5,
+//             numReviews: 10,
+//             description: 'high quality product',
+
+//         },
+//         {
+//             _id: '1',
+//             name: 'Nike Slim shirts',
+//             category: 'Shirts',
+//             Image: '/images/product1.jpg',
+//             price: 120,
+//             brand: 'Nike',
+//             rating: 4.5,
+//             numReviews: 10,
+//             description: 'high quality product',
+
+//         },
+//         {
+//             _id: '1',
+//             name: 'Nike Slim shirts',
+//             category: 'Shirts',
+//             Image: '/images/product1.jpg',
+//             price: 120,
+//             brand: 'Nike',
+//             rating: 4.5,
+//             numReviews: 10,
+//             description: 'high quality product',
+
+//         }, {
+//             _id: '1',
+//             name: 'Nike Slim shirts',
+//             category: 'Shirts',
+//             Image: '/images/product1.jpg',
+//             price: 120,
+//             brand: 'Nike',
+//             rating: 4.5,
+//             numReviews: 10,
+//             description: 'high quality product',
+
+//         },
+//         {
+//             _id: '1',
+//             name: 'Nike Slim shirts',
+//             category: 'Shirts',
+//             Image: '/images/product1.jpg',
+//             price: 120,
+//             brand: 'Nike',
+//             rating: 4.5,
+//             numReviews: 10,
+//             description: 'high quality product',
+
+//         },
+
+//     ],
+// };
